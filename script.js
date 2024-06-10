cb34c98727a92678c3f8da31dd52cbd08d02e04b
Ana Luiza Martins <ana.martins35@fatec.sp.gov.br>
2024-06-10 16:49:47 -0300

    closes #3 - error 404

--- script.js
+++ script.js
@@ -6,13 +6,35 @@ function search() {
     var url = `https://api.github.com/users/${username}`;
 
     $.getJSON(url, (user) => {
-
-        document.getElementById("name").innerHTML = user.name;
-        document.getElementById("html_url").innerHTML = user.html_url;
-        document.getElementById("company").innerHTML = user.company;
-        document.getElementById("avatar_url").innerHTML =`
-        <img  src=${user.avatar_url} width="220" height="220" class="shadow rounded">
-        `
+        showUserData(user);
+        clearError();
+    }).fail(() => {
+        showUserData({});
+        showError("Não encontrado!");
     });
-    
+
+}
+
+function showError(msg) {
+    document.getElementById("error").innerHTML = `<div class= 'alert alert-danger mt-1' role= 'alert'>${msg}</div>`;
+
+}
+
+function clearError() {
+    document.getElementById("error").innerHTML = "";
+
+}
+
+function showUserData(user) {
+
+    document.getElementById("name").innerHTML = user.name || "";
+    document.getElementById("html_url").innerHTML = user.html_url || "";
+    document.getElementById("company").innerHTML = user.company || "";
+
+    document.getElementById("avatar_url").innerHTML = user.avatar_url ?
+        `
+                                                     <img  src=${user.avatar_url} width="220" height="220" class="shadow rounded">
+                                                      ` :
+        "";
+
 }
